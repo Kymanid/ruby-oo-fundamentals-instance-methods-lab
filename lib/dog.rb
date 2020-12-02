722602e7a40b47afa2247c783fc727a558a92b1f
Kymani Davis <Kymanidavis@gmail.com>
2020-12-01 23:23:08 -0500

    commit tree e05959b6a1c52042ef5acb0069d7ecb2b3ac716e

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+end
+
+class Dog
+    def sit    
+        puts "The Dog is sitting"
+    end
+end
+
+
+
+Dog.new
+fido = Dog.new
+fido.bark 
+fido.sit
